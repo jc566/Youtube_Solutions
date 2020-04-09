0c54ecc17271f8e585399910220f695f2379295e
Joseph Choi <jc566@njit.edu>
2020-04-09 15:18:20 -0400

    adding replace files script

---
+++ Powershell Scripts/ReplaceFiles_UsingToken/ReplaceFiles_UsingToken.ps1
@@ -0,0 +1,31 @@
+﻿param(
+[string]$targetFiles = "", #target string to look for
+[string]$directory = ""
+)
+
+$filesToReplace = (Get-ChildItem -Path $directory -Filter *.$targetFiles* -Recurse).FullName
+
+foreach($file in $filesToReplace)
+{
+    #create a variable that finds a file with $targetFiles and replaces it with the "original" files name.
+
+    write-Host "Beginning file replacement for $file"
+    $originalFile = $file.replace(".$($targetFiles).",'.')
+
+    write-host "File Name is $originalFile"
+    if(test-path $originalFile)
+    {
+        remove-item $originalFile -force
+        write-host "Original file $originalFile is removed"
+    }
+    else
+    {
+        write-warning "Original File, $originalFile , is not found" 
+    }
+
+    if(Test-Path $file)
+    {
+        Write-Host "$file is renamed to $originalFile"
+        Rename-Item $file $originalFile -force
+    }
+}
