8cb1ec31377971c1c8ff30b93ea6f7b10949e5a2
Joseph Choi <jc566@njit.edu>
2020-06-15 21:27:43 -0400

    uploading REST api calls scripts

---
+++ Powershell Scripts/REST API intro/RestAPI Calls - No Auth Token.ps1
@@ -0,0 +1,14 @@
+﻿$url_base = "https://cat-fact.herokuapp.com"
+$url_endpoint = "/facts"
+$url = $url_base + $url_endpoint
+
+$response = Invoke-RestMethod -uri $url -Method Get -ContentType "application/json" -headers $header
+
+#option 1 for display/utilization
+foreach($item in $response.all)
+{
+$item
+}
+
+#option 2 for display/utilization
+$response | ConvertTo-Json #-Depth 4
---
+++ Powershell Scripts/REST API intro/RestAPI Calls - With Auth Token.ps1
@@ -0,0 +1,14 @@
+﻿#https://docs.microsoft.com/en-us/rest/api/azure/devops/distributedtask/variablegroups/get%20variable%20groups?view=azure-devops-rest-5.1
+
+$url_base = "https://dev.azure.com/YoutubeVids/Powershell_RestAPI/"
+$url_endpoint = "_apis/distributedtask/variablegroups?api-version=5.1-preview.1"
+$url = $url_base + $url_endpoint
+$Personal_Access_Token = "YOUR_ACCESS_TOKEN"
+$user = ""
+
+$token = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $user, $Personal_Access_Token)))
+$header = @{authorization = "Basic $token"}
+
+$response = Invoke-RestMethod -uri $url -Method Get -ContentType "application/json" -headers $header
+
+$response | ConvertTo-Json -Depth 4
