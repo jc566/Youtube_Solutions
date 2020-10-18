f551215abcabf1c65cd75cfb0360cb655e6a1e19
Joe Choi <Jc566@njit.edu>
2020-10-18 13:08:46 -0400

    Using ACL And powershell for security settings

---
+++ Powershell Scripts/Set Security for Files & Folders/Using ACL and powershell.ps1
@@ -0,0 +1,10 @@
+﻿$folderPath = "C:\Users\Joe\Downloads\TEST_FOLDER_2020"
+$user = "ExampleUser1"
+
+$acl = Get-Acl $folderPath
+
+$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("$user","FullControl","Allow")
+
+$acl.SetAccessRule($AccessRule)
+
+$acl | Set-Acl $folderPath
