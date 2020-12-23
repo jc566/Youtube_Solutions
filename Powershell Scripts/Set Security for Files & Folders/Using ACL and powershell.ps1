db8015237f5682f25f569f4bf2ee20ef97a8bbc9
Joe Choi <Jc566@njit.edu>
2020-12-23 12:03:42 -0500

    added files for security permissions and powershell

--- Powershell Scripts/Set Security for Files & Folders/Using ACL and powershell.ps1
+++ Powershell Scripts/Set Security for Files & Folders/Using ACL and powershell.ps1
@@ -1,10 +1,21 @@
-﻿$folderPath = "C:\Users\Joe\Downloads\TEST_FOLDER_2020"
+﻿$folderPath = "C:\Users\Joe\Documents\YT Video Folder\testFolder"
+
 $user = "ExampleUser1"
+
+$accesstype = "FullControl"
+
+$allowOrDeny = "Allow"
+
+$argList = $user,$accesstype,$allowOrDeny
 
 $acl = Get-Acl $folderPath
 
-$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("$user","FullControl","Allow")
+$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule -ArgumentList $argList
 
 $acl.SetAccessRule($AccessRule)
 
 $acl | Set-Acl $folderPath
+
+#https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.security/set-acl?view=powershell-7.1
+
+#https://docs.microsoft.com/en-us/dotnet/api/system.security.accesscontrol.filesystemrights?view=net-5.0
---
+++ Powershell Scripts/Set Security for Files & Folders/Using ICACLS and powershell.ps1
@@ -0,0 +1,15 @@
+﻿$folderPath = "C:\Users\Joe\Documents\YT Video Folder\testFolder"
+
+$user = "ExampleUser1"
+
+$grant = "/grant:r"
+
+$permission = ":(OI)(CI)(F)"
+
+$inheritance = "/inheritance:e"
+
+Invoke-Expression -Command ('icacls $folderPath $inheritance $grant "${user}${permission}"')
+
+#https://ss64.com/nt/icacls.html
+
+#https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/icacls
