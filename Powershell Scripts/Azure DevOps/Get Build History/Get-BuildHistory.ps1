1fceadbc86e3f194d4869705de004cda554e8feb
Joe Choi <Jc566@njit.edu>
2021-02-07 21:35:30 -0500

    added build pipeline pp and install ps module script

---
+++ Powershell Scripts/Azure DevOps/Get Build History/Get-BuildHistory.ps1
@@ -0,0 +1,59 @@
+﻿#Azure Devops params
+$AzureDevops_org = "DevopsTest00001"
+$AzureDevops_project = "CICD"
+
+#API Calls
+$url_base = "https://dev.azure.com/"
+$url_endpoint = "$AzureDevops_org/$AzureDevops_project/_apis/build/builds?api-version=6.1-preview.6"
+$url = $url_base + $url_endpoint
+$Personal_Access_Token = "6tomaatne3ek4uhf7mjwwjaeje5mrkte2jv3usvyk2igmvhlnskq"
+$user = ""
+
+$token = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $user, $Personal_Access_Token)))
+$header = @{authorization = "Basic $token"}
+
+$response = Invoke-RestMethod -uri $url -Method Get -ContentType "application/json" -headers $header
+
+#Class
+class BuildInfo 
+{
+    [string]$BuildName
+    [string]$BuildID
+    [string]$Status
+    [string]$Result
+}
+
+#Declare Arraylist to easily add/remove
+$Builds = [System.Collections.ArrayList]@()
+
+#Populate the Arraylist with instances based on the class we created earlier
+foreach($item in $response.value)
+{
+    $build = [BuildInfo]::new()
+    $build.BuildName = $item.definition.name
+    $build.BuildID = $item.definition.id
+    $build.Status = $item.status
+    $build.Result = $item.result
+
+    $Builds.Add($build) | Out-null #if you comment out 'Out-Null' you will see that this outputs the Count of items. Do this to remove the gibberish
+}
+
+#Filter the list for Unique values only
+$Builds = $Builds | Select-Object BuildName,BuildID,Status,Result -Unique
+
+#Display the information we care about
+for ($i=0;$i -lt $Builds.Count; $i++)
+{
+    write-host "
+                Build Name :  $($Builds[$i].BuildName) `n 
+                Build ID : $($Builds[$i].BuildID) `n 
+                Build Status : $($Builds[$i].status) `n
+                Build Result : $($Builds[$i].result) `n"
+}
+
+
+#Declare the output file name
+$output_file = "C:\Users\Joe\Documents\SampleJson.Json"
+
+#Write the entire json object retrieved into a output file
+$response | ConvertTo-Json -Depth 4 | Out-File $output_file
---
+++ Powershell Scripts/Install PS Module/InstallPSModule.ps1
@@ -0,0 +1,34 @@
+﻿function InstallPSModule
+{
+    param(
+        [string]$moduleName,
+        [string]$scope
+    )
+    try
+    {
+        if(Get-Module -ListAvailable -Name $moduleName)
+        {
+            Import-Module $moduleName
+        }
+        else
+        {
+            Install-Module $moduleName -Scope $scope -Force -AllowClobber
+
+            Import-Module $moduleName
+        }
+    }
+    catch [Exception]
+    {
+        write-host $_.Exception.Message
+    }
+    catch
+    {
+        write-host "$($moduleName) was not found and was unable to be installed"
+    }
+    finally
+    {
+        write-host "Completed $($moduleName) Check/Installation"
+    }
+}
+
+InstallPSModule -moduleName "" -scope ""
