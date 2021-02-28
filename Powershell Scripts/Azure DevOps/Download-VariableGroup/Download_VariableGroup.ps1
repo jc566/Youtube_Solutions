3160e58dc60a199c4133e1f6c197b675fa70a9c8
Joe Choi <Jc566@njit.edu>
2021-02-28 17:07:46 -0500

    generalized the scrips parameters

--- Powershell Scripts/Azure DevOps/Download-VariableGroup/Download_VariableGroup.ps1
+++ Powershell Scripts/Azure DevOps/Download-VariableGroup/Download_VariableGroup.ps1
@@ -1,12 +1,12 @@
 #Generate URL
-$organization = "DevopsTest00001"
-$project = "CICD"
+$organization = "[YOUR ORGANIZATION]"
+$project = "[YOUR PROJECT]"
 $url_base = "https://dev.azure.com/$organization/$project/"
 $url_endpoint = "_apis/distributedtask/variablegroups?api-version=6.0-preview.2"
 $url = $url_base + $url_endpoint
 
 #Generate PAT
-$Personal_Access_Token= "32gipqzgaijwfz4ij6clx5riylf5waaqrcwa7pxeeh4e5jnzkj4a"
+$Personal_Access_Token= "YOUR PERSONAL ACCESS TOKEN"
 $user = ""
 $token = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $user, $Personal_Access_Token)))
 $header = @{authorization = "Basic $token"}
@@ -14,5 +14,5 @@ $header = @{authorization = "Basic $token"}
 $response = Invoke-RestMethod -Uri "$url" -Method GET -ContentType "application/json" -headers $header | convertto-json -depth 100
 
 #Save response into JSON file locally
-$output = "C:\Users\Joe\Documents\YT Video Folder\testFolder\jsonobject.json"
+$output = "YOUR LOCAL PATH"
 $response | out-file $output
