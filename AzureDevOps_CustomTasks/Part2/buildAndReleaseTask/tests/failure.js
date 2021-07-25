779bccf2645e1465ca7ff3077d3341967029b0d3
Joe Choi <Jc566@njit.edu>
2021-07-25 18:35:44 -0400

    updated and finished video

---
+++ AzureDevOps_CustomTasks/Part2/buildAndReleaseTask/tests/failure.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var tmrm = require("azure-pipelines-task-lib/mock-run");
+var path = require("path");
+var taskPath = path.join(__dirname, '..', 'index.js');
+var tmr = new tmrm.TaskMockRunner(taskPath);
+tmr.setInput('samplestring', 'bad');
+tmr.run();
