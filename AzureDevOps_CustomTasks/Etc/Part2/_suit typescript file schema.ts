cbfd6c084211708c5c337f6f953ba628ba39deec
Joe Choi <Jc566@njit.edu>
2021-07-25 17:32:30 -0400

    added some peices for part 2

---
+++ AzureDevOps_CustomTasks/Etc/Part2/_suit typescript file schema.ts
@@ -0,0 +1,22 @@
+import * as path from 'path';
+import * as assert from 'assert';
+import * as ttm from 'azure-pipelines-task-lib/mock-test';
+
+describe('Sample task tests', function () {
+
+    before( function() {
+
+    });
+
+    after(() => {
+
+    });
+
+    it('should succeed with simple inputs', function(done: Mocha.Done) {
+        // Add success test here
+    });
+
+    it('it should fail if tool returns 1', function(done: Mocha.Done) {
+        // Add failure test here
+    });    
+});
