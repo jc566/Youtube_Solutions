633c5fd6305c99cf53dad383c2a1da6661356c4f
Joe Choi <Jc566@njit.edu>
2023-03-29 09:32:10 -0400

    Added ARM Linked Templates project files

---
+++ Azure ARM/ARM Linked Templates/00-Create_OR_Delete_RGs/Create-BaseRGs.ps1
@@ -0,0 +1,7 @@
+#Create Example Test RGs
+az group create --name CodeDogeYTASP --location eastus
+az group create --name CodeDogeYTRG00 --location eastus
+az group create --name CodeDogeYTRG01 --location eastus
+az group create --name CodeDogeYTRG02 --location eastus
+az group create --name CodeDogeYTRG03 --location eastus
+az group create --name CodeDogeYTRG04 --location eastus
---
+++ Azure ARM/ARM Linked Templates/00-Create_OR_Delete_RGs/Delete-BaseRGs.ps1
@@ -0,0 +1,7 @@
+# Delete Example Test RGs
+az group delete --name CodeDogeYTASP
+az group delete --name CodeDogeYTRG00
+az group delete --name CodeDogeYTRG01
+az group delete --name CodeDogeYTRG02
+az group delete --name CodeDogeYTRG03
+az group delete --name CodeDogeYTRG04
